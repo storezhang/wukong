022791652060710518d4617f03d6f1f091f9a864
storezhang <storezhang@gmail.com>
2020-11-25 23:00:12 +0800

    增加Gob序列化

--- serializer_json.go
+++ serializer_json.go
@@ -1,7 +1,10 @@
 package wukong
 
 import (
+	`bytes`
 	`encoding/json`
+	`reflect`
+	`unsafe`
 )
 
 type SerializerJson struct{}
@@ -10,6 +13,24 @@ func (sj *SerializerJson) Marshal(obj interface{}) ([]byte, error) {
 	return json.Marshal(obj)
 }
 
-func (sj *SerializerJson) Unmarshal(data []byte, obj interface{}) error {
-	return json.Unmarshal(data, obj)
+func (sj *SerializerJson) Unmarshal(data []byte) (ptr interface{}, err error) {
+	buffer := bytes.NewBuffer(data)
+	decoder := json.NewDecoder(buffer)
+
+	var obj interface{}
+	if err = decoder.Decode(&obj); nil != err {
+		return
+	}
+
+	value := reflect.ValueOf(obj)
+	if value.Kind() == reflect.Struct {
+		var objPtr interface{} = &obj
+		interfaceData := reflect.ValueOf(objPtr).Elem().InterfaceData()
+		sp := reflect.NewAt(value.Type(), unsafe.Pointer(interfaceData[1])).Interface()
+		ptr = sp
+	} else {
+		ptr = obj
+	}
+
+	return
 }
---
+++ serializer_json_test.go
@@ -0,0 +1,32 @@
+package wukong
+
+import (
+	`fmt`
+	`testing`
+)
+
+var (
+	sj *SerializerJson
+)
+
+func TestJsonMarshal(t *testing.T) {
+	var (
+		data []byte
+		err  error
+	)
+
+	if data, err = sj.Marshal(&user{
+		Username: "storezhang",
+		Password: "test",
+		Age:      34,
+		Money:    2000000000000,
+	}); nil != err {
+		t.Fatalf("序列化出错：%s", err)
+	}
+
+	var userPtr interface{}
+	if userPtr, err = sj.Unmarshal(data); nil != err {
+		t.Fatalf("反序列化出错：%s", err)
+	}
+	fmt.Print(userPtr.(user))
+}
